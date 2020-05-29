2684458ca6e6ab49b8b2b37bd19748797acc7bf2
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-29 17:19:48 +0530

    addition of custom data via file

--- mongoclient.py
+++ mongoclient.py
@@ -3,9 +3,18 @@ from pymongo import MongoClient
 import datetime
 import pprint
 import argparse
+import json
 isTrue = lambda  val : True if val.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh', 'on'] else False
 
+class TestData:
+    post = {"author": "Mike4",
+            "text": "My first blog post!",
+            "tags": ["mongodb", "python", "pymongo"],
+            "date": datetime.datetime.utcnow()}
+    
+
 class MongoDBMgr:
+
     def __init__(self, ip='127.0.0.1', port=27017, db='test_database'):
       self.client = self.__connect_db(ip, port)
       self.db = self.client[db]
@@ -15,12 +24,8 @@ class MongoDBMgr:
         return MongoClient(ip, port)
         
 
-    def insert_test_data(self):
+    def insert_test_data(self, post = TestData.post):
         db = self.db
-        post = {"author": "Mike4",
-                 "text": "My first blog post!",
-                 "tags": ["mongodb", "python", "pymongo"],
-                 "date": datetime.datetime.utcnow()}
         posts = db.posts
         self.post_id= post_id = posts.insert_one(post).inserted_id
         print("Store it for query. post ID is {} ".format(post_id))
@@ -45,15 +50,21 @@ def main():
                            help='Mongo server IP', default='localhost')
     parser.add_argument('--port', metavar='N', type=int,
                            help='Mongo server Port', default=27017)
-    parser.add_argument('--add', metavar='N', type=str,
-                           help='Insert test data', default='no')
+    parser.add_argument('--post', metavar='N', type=str,
+                           help='Post test data', default='no')
+    parser.add_argument('--postdata', metavar='N', type=str,
+                           help='Post Data value', default=None)
     parser.add_argument('--fetch', metavar='N', type=str,
                            help='Check test data', default='yes')
     args = parser.parse_args()
     mongoDBMgr = MongoDBMgr(args.ip, args.port)
-    if isTrue(args.add.lower()):
+    postdata = TestData.post
+    if args.postdata:
+        postdata = json.loads(args.postdata)
+        postdata['date'] = datetime.datetime.utcnow()
+    if isTrue(args.post.lower()) or postdata:
         print("Inserting a test data")
-        mongoDBMgr.insert_test_data()
+        mongoDBMgr.insert_test_data(post = postdata)
     if isTrue(args.fetch.lower()):
         print("Fetching test data")
         mongoDBMgr.find_one()
