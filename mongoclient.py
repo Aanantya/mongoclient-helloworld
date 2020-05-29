ec13e0c03474230b863ab9a4afcea6cb7c44bee3
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-29 14:20:05 +0530

    Added code to fetch the same entry
    New features
    1. When user adds entry, same entry is fetched
    2. Else, count of entries is displayed

--- mongoclient.py
+++ mongoclient.py
@@ -9,6 +9,7 @@ class MongoDBMgr:
     def __init__(self, ip='127.0.0.1', port=27017, db='test_database'):
       self.client = self.__connect_db(ip, port)
       self.db = self.client[db]
+      self.post_id = None
   
     def __connect_db(self, ip='127.0.0.1', port=27017):
         return MongoClient(ip, port)
@@ -16,18 +17,26 @@ class MongoDBMgr:
 
     def insert_test_data(self):
         db = self.db
-        post = {"author": "Mike",
+        post = {"author": "Mike4",
                  "text": "My first blog post!",
                  "tags": ["mongodb", "python", "pymongo"],
                  "date": datetime.datetime.utcnow()}
         posts = db.posts
-        post_id = posts.insert_one(post).inserted_id
+        self.post_id= post_id = posts.insert_one(post).inserted_id
+        print("Store it for query. post ID is {} ".format(post_id))
 
     def find_one(self):
-       db = self.db
-       posts = db.posts
-       pprint.pprint(posts.find_one())
-       return
+        post_id = self.post_id         
+        db = self.db
+        posts = db.posts
+        if post_id:
+            query = {"_id": post_id}
+            pprint.pprint(posts.find_one(query))
+        else:
+            pprint.pprint(posts.find_one())
+        count = posts.count_documents({})
+        print("Current number of entries are {}".format(count))
+        return
 	
 
 def main():
